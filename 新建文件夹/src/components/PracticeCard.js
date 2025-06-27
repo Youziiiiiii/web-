27a311a5d0bf7feba8ce0ac5cd1d8257a0f9ee5a
Youziiiiiii <2136675385@qq.com>
2025-06-28 00:12:15 +0800

    Delete 新建文件夹 directory

--- 新建文件夹/src/components/PracticeCard.js
+++
@@ -1,8 +0,0 @@
-export default function PracticeCard() {
-    return (
-        <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', borderRadius: '5px' }}>
-            <h2>西北民族大学</h2>
-            <p>web作业</p>
-        </div>
-    );
-}
