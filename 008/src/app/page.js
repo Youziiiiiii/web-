063530c7ae718be1d17fd18acdd9d4b18fecce12
Youziiiiiii <2136675385@qq.com>
2025-07-03 22:10:47 +0800

    8
    8

---
+++ 008/src/app/page.js
@@ -0,0 +1,14 @@
+import PracticeCard from "../components/PracticeCard"
+import Tset from "../components/Test"
+export default function Home() {
+  return (
+    <div>
+      <h1>我的主页</h1>
+      <h1>欢迎来到我的Next.js应用。</h1>
+      <Tset />
+      <PracticeCard />
+         <Tset />
+            <Tset />
+    </div>
+  )
+}
